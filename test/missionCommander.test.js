61feb79c541dee5fd9e76ad633bd5bc06a5e878f
RaulG <raul.animations@gmail.com>
2022-04-25 13:33:57 -0500

    Se agrega el proyecto inicial y sus primera prueba

---
+++ test/missionCommander.test.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./../app/missionCommander')
+
+describe("Esto es una suite de pruebas", () => {
+    test('Caso de prueba 1', () => {
+        const result = 8 + 2;
+        expect(result).toBe(10);
+    })
+})
